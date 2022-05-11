174dd2319948592394ec9c12bbfae0f7d934e888
polinaprinii <91224252+polinaprinii@users.noreply.github.com>
2022-05-11 20:26:36 +0100

    Update Data_Cleansing_Fertility_vs_Contraception.py

---
+++ Data_Intensive_Architectures/Data_Cleansing_Fertility_vs_Contraception.py
@@ -0,0 +1,6 @@
+"""
+This file is responsible for the data cleansing of the fertility-vs-contraception file.
+The following steps are applied:
+- All records for continents are removed.
+- Continent added to each country record.
+"""
