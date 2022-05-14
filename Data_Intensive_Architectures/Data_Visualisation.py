ee9a03e6ee0581724f39b69653f3798da06f01c8
polinaprinii <x21137757@student.ncirl.ie>
2022-05-15 00:40:55 +0100

    Linear regression
    Output data tested against linear regression to identify if relationship is present.

--- Data_Intensive_Architectures/Data_Visualisation.py
+++ Data_Intensive_Architectures/Data_Visualisation.py
@@ -1,6 +1,24 @@
 import pandas as pd
 import seaborn as sns
 sns.set_theme()
-import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
+import scipy.stats as stats
+
+# Importing our dataframe:
+df = pd.read_csv('/Users/polinaprinii/Documents/GitHub/Data-Intensive-Architecture-Final-Project/Data_Intensive_Architectures/Output/Final_Data.csv'
+                 )
+
+# Assigning our x (independent) and y dependent variables.
+x = df['Average Fertility Rate'].values
+y = df['Total Number of Deaths'].values
+
+
+# If you need the model coefficients stored in variables for later use, do:
+model = stats.linregress( x, y )
+beta0 = model.intercept
+beta1 = model.slope
+
+# If you just need to see the coefficients (and some other related data),
+# do this alone:
+print('The R-squared value for the linear relationship between "Total Number of Deaths" and "Average Fertility Rate" is: ',
+      round(model.rvalue, 2), '\n')
 
